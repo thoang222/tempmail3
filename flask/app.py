de644fb053e7fdd1663225f7c7e72f2f71a1d50e
thoang222 <156932337+thoang222@users.noreply.github.com>
2024-02-16 05:36:09 +0700

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-import subprocess
+import subprocess, os
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -17,6 +18,25 @@ def execute_command(command):
         return result.stdout
     except Exception as e:
         return str(e)
+
+@app.route('/view_mail', methods=['GET'])
+def read_file():
+    file_path = '/tmp/tmpmail/tmpmail.html'
+    try:
+        if os.path.isfile(file_path):
+            with open(file_path, 'r') as file:
+                file_content = file.read()
+            soup = BeautifulSoup(file_content, 'html.parser')
+            tiktok_link = soup.find('a', {'href': lambda x: x and 'api.tiktokv.com' in x})
+            if tiktok_link:
+                href_value = tiktok_link.get('href')
+                return href_value
+            else:
+                return None
+        else:
+            return f"File '{file_path}'not have"
+    except Exception as e:
+        return f"Error: {e}"
 
 if __name__ == '__main__':
     app.run(debug=True)
