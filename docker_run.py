c4960ff4cc8ccbafe30c996031edf003757875e6
thoang <thoang>
2024-02-17 04:56:55 +0700

    new

---
+++ docker_run.py
@@ -0,0 +1,26 @@
+import docker
+
+def build_image_from_dockerfile():
+    client = docker.from_env()
+    image, build_logs = client.images.build(path="./flask", dockerfile="Dockerfile", tag="flask")
+
+    for log in build_logs:
+        if 'stream' in log:
+            print(log['stream'], end='')
+
+    return image
+
+def run_container(image, port):
+    client = docker.from_env()
+    ports = {'{0}/tcp'.format(port): port}
+    container = client.containers.run(image, ports=ports, detach=True)
+    print(f'Container {container.id} is running on port {port}')
+
+if __name__ == "__main__":
+    docker_image = build_image_from_dockerfile()
+
+    if docker_image:
+        for port in range(5000, 5021):
+            run_container(docker_image.tags[0], port)
+    else:
+        print("Failed to build Docker image.")
