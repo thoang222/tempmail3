2cb43e56e2132c18fb059fe8258ceb5bec305a37
thoang <thoang>
2024-02-17 06:08:29 +0700

    hello

--- docker_run.py
+++ docker_run.py
@@ -12,15 +12,19 @@ def build_image_from_dockerfile():
 
 def run_container(image, port):
     client = docker.from_env()
+    
+    # Thiết lập giá trị của biến môi trường PORT để chuyển đến container
+    environment = {'PORT': port}
+    
     ports = {'{0}/tcp'.format(port): port}
-    container = client.containers.run(image, ports=ports, detach=True)
+    container = client.containers.run(image, ports=ports, detach=True, environment=environment)
     print(f'Container {container.id} is running on port {port}')
 
 if __name__ == "__main__":
     docker_image = build_image_from_dockerfile()
 
     if docker_image:
-        for port in range(5000, 5021):
+        for port in range(5000, 5006):
             run_container(docker_image.tags[0], port)
     else:
         print("Failed to build Docker image.")
